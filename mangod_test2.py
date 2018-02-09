e2e26d2e97c12fb72347ec133e9daf334deecd16
Janny <janny@Jannys-MacBook-Pro.local>
2018-02-09 18:20:33 +0800

    包装了一下

--- mangod_test2.py
+++ mangod_test2.py
@@ -3,27 +3,12 @@ from pymongo import MongoClient
 import copy
 import pprint
 import datetime
-#建立MongoDB数据库连接
-client=MongoClient('localhost',27017)
+import re
 
-#连接所需数据库，为数据库名
-db=client.bello_annotation
-#连接所需集合，也就是我们通常所说的表，taxonomy为表
-collection=db.banchmark
-schema=db.schema
-
-# =============================================================================
-# #制作schema的dict方便查询(非boolean非text)
-# schema_dict={}
-# keys=schema.find_one()['properties'].keys()
-# for i in keys:
-#     if schema.find_one()['properties'][i]['type']!='boolean' and schema.find_one()['properties'][i]['type']!='text':
-#         print(i,':',schema.find_one()['properties'][i]['type'])
-#         schema_dict[i]=schema.find_one()['properties'][i]['type']
-# =============================================================================
 def to_array(content):
     result_num=[]
-    result_num.append(str(content))
+    for i in re.split('[,，/\、-]',content):
+        result_num.append(str(i))
     return result_num
 
 def toString(content):
@@ -32,8 +17,10 @@ def toString(content):
 def to_number(content):
     if content.strip()=='':
         return None
+    elif isinstance(content,float):
+        return content
     else:
-        return float(content)
+        return int(content)
 
 def skill_type_correct(content):
     return {
@@ -42,9 +29,24 @@ def skill_type_correct(content):
             }.get(content,None)
     
     
+#建立MongoDB数据库连接
+client=MongoClient('localhost',27017)
+#连接所需数据库，bello_annotation为数据库名
+db=client.bello_annotation
+#连接所需集合
+collection=db.test_small
 post=collection.find_one()
-corrected_result= copy.deepcopy(post['results'])
 original_result=post['results']
+corrected_result= copy.deepcopy(post['results'])
+
+# #制作schema的dict，方便查询(非boolean非text)
+# schema=db.schema
+# schema_dict={}
+# keys=schema.find_one()['properties'].keys()
+# for i in keys:
+#     if schema.find_one()['properties'][i]['type']!='boolean' and schema.find_one()['properties'][i]['type']!='text':
+#         print(i,':',schema.find_one()['properties'][i]['type'])
+#         schema_dict[i]=schema.find_one()['properties'][i]['type']
 
 #location 成功测试
 for i in range(len(corrected_result)):
@@ -57,6 +59,7 @@ for i in range(len(corrected_result)):
   except:
     continue
 
+#YearsOfExperience 成功测试
 YearOfExperience=['minYearsOfExperience','maxYearsOfExperience','minYearOfInternshipWorkExp','maxYearOfInternshipWorkExp','minYearOfManagementWorkExp','maxYearOfManagementWorkExp','minYearOfRelevantWorkExp','maxYearOfRelevantWorkExp']
 def year_of_exp_correct(exp_category):
     for i in range(len(corrected_result)):
@@ -73,30 +76,30 @@ def year_of_exp_correct(exp_category):
 for exp_category in YearOfExperience:
     year_of_exp_correct(exp_category)
 
-# #minYearsOfExperience 成功测试
+def degree_major_correct(degree_major_type):
+    for i in range(len(corrected_result)):
+        try:
+            result_temp = corrected_result[i]["labelledData"][degree_major_type]
+            if not type(result_temp) == list:
+                print(result_temp)
+                result_num=to_array(result_temp)
+                corrected_result[i]['labelledData'][degree_major_type]=result_num
+        except:
+            continue    
+degree_major = ['requiredDegreeMajor','relevantDegreeMajor']
+for degree_major_type in degree_major:
+    degree_major_correct(degree_major_type)
+
+# #requiredDegreeMajor 成功测试
 # for i in range(len(corrected_result)):
-#     try:
-#         result_temp = corrected_result[i]["labelledData"]['minYearsOfExperience']
-#     #    if not isinstance(result_temp,int) and not isinstance(result_temp,float):
-#         if isinstance(result_temp,str):
-#             print('strContent:',result_temp)
-#             result_num=to_number(result_temp)
-#             print(result_num)
-#             corrected_result[i]['labelledData']['minYearsOfExperience']=result_num
-#     except:
-#         continue
-
-
-#requiredDegreeMajor 成功测试
-for i in range(len(corrected_result)):
-  try:
-    result_temp = corrected_result[i]["labelledData"]['requiredDegreeMajor']
-    if not type(result_temp) == list:
-      print(result_temp)
-      result_num=to_array(result_temp)
-      corrected_result[i]['labelledData']['requiredDegreeMajor']=result_num
-  except:
-    continue
+#   try:
+#     result_temp = corrected_result[i]["labelledData"]['requiredDegreeMajor']
+#     if not type(result_temp) == list:
+#       print(result_temp)
+#       result_num=to_array(result_temp)
+#       corrected_result[i]['labelledData']['requiredDegreeMajor']=result_num
+#   except:
+#     continue
 
 #'requiredSkills','otherSkills','optionalSkills' 成功测试
 #修改  ‘hardSkill -> nski’;'softSkill'->'nsks'
@@ -111,34 +114,7 @@ def skill_correct(skillCategory):
 
 skillCategory=['requiredSkills','otherSkills','optionalSkills']
 for i in skillCategory:
-    skill_correct(i)
-
-# #requiredSkills 成功测试    
-# for i in range(len(corrected_result)):
-#     try:
-#         requiredSkills_many = corrected_result[i]["labelledData"]['requiredSkills']
-#         for j in range(len(requiredSkills_many)):
-#             requiredSkills_many[j]['skillType']=skill_type_correct(requiredSkills_many[j]['skillType'])
-#     except:
-#         continue
-
-# #otherSkills 成功测试    
-# for i in range(len(corrected_result)):
-#     try:
-#         otherSkills_many = corrected_result[i]["labelledData"]['otherSkills']
-#         for j in range(len(otherSkills_many)):
-#             otherSkills_many[j]['skillType']=skill_type_correct(otherSkills_many[j]['skillType'])
-#     except:
-#         continue
-
-# #optionalSkills 成功测试    
-# for i in range(len(corrected_result)):
-#     try:
-#         optionalSkills_many = corrected_result[i]["labelledData"]['optionalSkills']
-#         for j in range(len(optionalSkills_many)):
-#             optionalSkills_many[j]['skillType']=skill_type_correct(optionalSkills_many[j]['skillType'])
-#     except:
-#         continue        
+    skill_correct(i)       
 
 print('#############################')
 # for j in range(len(original_result)):
