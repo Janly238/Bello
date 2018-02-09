2305b4394fb57426e66bb05aea2309bf4d6138af
Janny <janny@Jannys-MacBook-Pro.local>
2018-02-10 00:19:27 +0800

    给璨哥的第一版V2.10

--- mangod_test2.py
+++ mangod_test2.py
@@ -34,7 +34,7 @@ client=MongoClient('localhost',27017)
 #连接所需数据库，bello_annotation为数据库名
 db=client.bello_annotation
 #连接所需集合
-collection=db.test_small
+collection=db.banchmark
 post=collection.find_one()
 original_result=post['results']
 corrected_result= copy.deepcopy(post['results'])
@@ -50,16 +50,17 @@ corrected_result= copy.deepcopy(post['results'])
 
 #location 成功测试
 for i in range(len(corrected_result)):
-  try:
-    result_temp = corrected_result[i]["labelledData"]['location']
-    if not type(result_temp) == list:
-      print(result_temp)
-      result_num=to_array(result_temp)
-      corrected_result[i]['labelledData']['location']=result_num
-  except:
-    continue
+    try:
+        result_temp = corrected_result[i]["labelledData"]['location']
+        if not type(result_temp) == list:
+            print(result_temp)
+            result_num=to_array(result_temp)
+            corrected_result[i]['labelledData']['location']=result_num
+    except:
+        continue
 
 #YearsOfExperience 成功测试
+#它们均为number类型
 YearOfExperience=['minYearsOfExperience','maxYearsOfExperience','minYearOfInternshipWorkExp','maxYearOfInternshipWorkExp','minYearOfManagementWorkExp','maxYearOfManagementWorkExp','minYearOfRelevantWorkExp','maxYearOfRelevantWorkExp']
 def year_of_exp_correct(exp_category):
     for i in range(len(corrected_result)):
@@ -76,6 +77,9 @@ def year_of_exp_correct(exp_category):
 for exp_category in YearOfExperience:
     year_of_exp_correct(exp_category)
 
+#'requiredDegreeMajor','relevantDegreeMajor' 成功测试
+# 两者为array类型
+degree_major = ['requiredDegreeMajor','relevantDegreeMajor']
 def degree_major_correct(degree_major_type):
     for i in range(len(corrected_result)):
         try:
@@ -86,23 +90,12 @@ def degree_major_correct(degree_major_type):
                 corrected_result[i]['labelledData'][degree_major_type]=result_num
         except:
             continue    
-degree_major = ['requiredDegreeMajor','relevantDegreeMajor']
 for degree_major_type in degree_major:
     degree_major_correct(degree_major_type)
 
-# #requiredDegreeMajor 成功测试
-# for i in range(len(corrected_result)):
-#   try:
-#     result_temp = corrected_result[i]["labelledData"]['requiredDegreeMajor']
-#     if not type(result_temp) == list:
-#       print(result_temp)
-#       result_num=to_array(result_temp)
-#       corrected_result[i]['labelledData']['requiredDegreeMajor']=result_num
-#   except:
-#     continue
-
 #'requiredSkills','otherSkills','optionalSkills' 成功测试
 #修改  ‘hardSkill -> nski’;'softSkill'->'nsks'
+skillCategory=['requiredSkills','otherSkills','optionalSkills']
 def skill_correct(skillCategory):
     for i in range(len(corrected_result)):
         try:
@@ -111,29 +104,26 @@ def skill_correct(skillCategory):
                 skillCategory_many[j]['skillType']=skill_type_correct(skillCategory_many[j]['skillType'])
         except:
             continue
-
-skillCategory=['requiredSkills','otherSkills','optionalSkills']
 for i in skillCategory:
     skill_correct(i)       
 
-print('#############################')
-# for j in range(len(original_result)):
-#   try:  
-#     print(type(original_result[j]['labelledData']['requiredDegreeMajor']),type(corrected_result[j]['labelledData']['requiredDegreeMajor']))
-#     print(original_result[j]['labelledData']['requiredDegreeMajor'])
-#     print(corrected_result[j]['labelledData']['requiredDegreeMajor'])
-# #    print(post1['results'][j]['labelledData']['requiredDegreeMajor'])
-#   except:
-#     continue
-    
-#collection.update({},{"results":corrected_result})
-#post1=collection.find_one()
-for i in range(len(corrected_result)):
-    try:#print(post['results'][j]['id'])
-        requiredSkills_many=corrected_result[i]['labelledData']['optionalSkills']
-        for j in range(len(requiredSkills_many)):
-            print(requiredSkills_many[j]['skillType'])
-        #print(type(original_result[i]['parsedData']['requiredDegreeMajor']),post['results'][i]['parsedData']['requiredDegreeMajor'])
-    except:
-        continue    
+#更新数据库 update DBS    
+collection.update({},{"results":corrected_result})
+post1=collection.find_one() #post1为更新数据库后最新的集合
 
+#测试代码
+#因为有些字段是没有的，所以加了异常处理，print的时候最好只print一个，不然容易出现因为其他字段是空导致整个循环会跳过的情况
+print('#############################')
+for i in range(len(corrected_result)):
+    print('------')
+    try:
+        # print('original_location:',original_result[i]['labelledData']['location'],'corrected_location:',corrected_result[i]['labelledData']['location'])
+        # print('original_minYearOfEXP:',original_result[i]['labelledData']['minYearsOfExperience'],'corrected_minYearOfEXP:',corrected_result[i]['labelledData']['minYearsOfExperience'])
+        # print('original_requiredDegreeMajor:',original_result[i]['labelledData']['requiredDegreeMajor'],'corrected_requiredDegreeMajor:',corrected_result[i]['labelledData']['requiredDegreeMajor'])
+        print(original_result[i]['labelledData']['location'],post1['results'][i]['labelledData']['location'])
+        # for skilltype in skillCategory: 
+        #     requiredSkills_many=corrected_result[i]['labelledData'][skilltype]
+        #     for j in range(len(requiredSkills_many)):
+        #         print(requiredSkills_many[j]['skillType'])
+    except:
+        continue 
